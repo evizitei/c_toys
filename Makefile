5e0f255b8f900fdf8a177adc637d991188a6e37e
Ethan Vizitei <ethan.vizitei@gmail.com>
2017-06-09 09:15:30 -0700

    implement triplet comparison

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@
 compile:
 	gcc algo/sum.c -o target/sum
 	gcc algo/sum_arr.c -o target/sum_arr
+	gcc algo/compare_triplets.c -o target/compare_triplets
---
+++ algo/compare_triplets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int *tripCompare(int *trips){
+  int *output = calloc(sizeof(int) * 2, sizeof(int));
+  for(int i =0; i < 3; i++){
+    int valA = *(trips + i);
+    int valB = *(trips + i + 3);
+    if(valA > valB){
+      *(output) = *(output) + 1;
+    }else if(valB > valA){
+      *(output + 1) = *(output + 1) + 1;
+    }
+  }
+  return output;
+}
+
+int main(){
+  int tripsCount = 2;
+  int valueCount = 3;
+  int elementCount = tripsCount * valueCount;
+  int *trips = malloc(sizeof(int) * elementCount);
+  for(int i = 0; i < elementCount; i++){
+    scanf("%i", &trips[i]);
+  }
+  int *output = tripCompare(trips);
+  printf("%d %d\n", *(output), *(output + 1));
+}
